b26a62a842bf1f0e9623fe163fe19bcaf3f6ed67
varianwrynn <13040839537@163.com>
2019-05-02 01:16:25 +0800

    添加阿里规约代码检查以及sequare java代码风格检查，并改正。

--- app/src/main/java/com/cornucopia/github/login/LoginActivity.java
+++ app/src/main/java/com/cornucopia/github/login/LoginActivity.java
@@ -1,20 +1,21 @@
-package com.cornucopia.github;
+package com.cornucopia.github.login;
 
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.cornucopia.github.R;
 
 /**
- * 主界面.
+ * 登录界面.
  *
  * @author LIUWEI <a href="mailto:misayanice@gmail.com">Contact me.</a>
  * @version 1.0
  * @since 2019-05-02
  */
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_login);
   }
 }
